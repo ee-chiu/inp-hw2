a3062b41709cfd4c489abd10bb4176b6bda6e4ee
ee-chiu <80535163+ee-chiu@users.noreply.github.com>
2021-11-14 14:20:58 +0800

    exit implemented

--- hw2.cpp
+++ hw2.cpp
@@ -3,11 +3,17 @@
 #include <arpa/inet.h>
 #include <stdio.h>
 #include <sys/select.h>
+#include <vector>
+#include <string>
 
 using namespace std;
 
 char cli_buff[10000];
 char srv_buff[10000];
+const int max_size = 20;
+vector<bool> isLogin(max_size, false);
+vector<string> user(max_size, "");
+fd_set all_set;
 
 int char2int(char* c){
     int i = 0;
@@ -21,6 +27,37 @@ int char2int(char* c){
     return num;
 } 
 
+vector<string> split(string command){
+    vector<string> para;
+    string tmp;
+    int i = 0;
+    while(command[i] != 0){
+        if(command[i] == '"') {
+            i++;
+            break;
+        }
+        if(command[i] == ' '){
+            para.push_back(tmp);
+            tmp.clear();
+            i++;
+            while(command[i] == ' ')
+                i++;
+            continue;
+        }
+        tmp += command[i];
+        i++;
+    }
+
+    while(command[i] != 0 && command[i] != '"'){
+        tmp += command[i];
+        i++;
+    }
+    
+    if(!tmp.empty())
+        para.push_back(tmp);
+    return para;
+}
+
 void write2cli(int connfd,const char * message){
     snprintf(cli_buff, sizeof(cli_buff), "%s", message);
     Write(connfd, cli_buff, strlen(cli_buff));
@@ -32,6 +69,27 @@ void welcome(int connectfd){
     return;
 }
 
+void bbs_main(int sockfd){
+    Read(sockfd, srv_buff, sizeof(srv_buff));
+    if(srv_buff[0] != 0){
+        string command(srv_buff);
+        command.pop_back();
+        if(command == "exit"){
+            if(isLogin[sockfd]) {
+                snprintf(cli_buff, sizeof(cli_buff), "Bye, %s.", user[sockfd].c_str());
+                Write(sockfd, cli_buff, strlen(cli_buff));
+            }
+            isLogin[sockfd] = false;
+            user[sockfd].clear();                
+
+            Close(sockfd);
+
+            FD_CLR(sockfd, &all_set);
+            return;
+        }
+    }
+}
+
 int main(int argc, char** argv){
     if(argc != 2){
         printf("Usage: ./hw2 <port>\n");
@@ -40,7 +98,6 @@ int main(int argc, char** argv){
 
     int listenfd, connectfd;
     struct sockaddr_in srv_addr;
-    const int max_size = 20;
     
     int port = char2int(argv[1]);
     bzero(&srv_addr, sizeof(srv_addr));
@@ -49,25 +106,20 @@ int main(int argc, char** argv){
     srv_addr.sin_port = htons(port);
     
     listenfd = Socket(AF_INET, SOCK_STREAM, 0);    
-    setsockopt(listenfd, SOL_SOCKET, (SO_REUSEADDR | SO_REUSEPORT), NULL, 0);
 
     Bind(listenfd, (struct sockaddr *) &srv_addr, sizeof(srv_addr));
 
     const int backlog = max_size;
     Listen(listenfd, backlog);
 
-    fd_set all_set;
     FD_ZERO(&all_set);
     FD_SET(listenfd, &all_set);
 
-    int cli[max_size];
+    vector<int> cli(max_size, -1);
     int i = 0;
     int maxi = -1;
     int maxfd = listenfd;
     
-    for(i = 0 ; i < max_size ; i++)
-        cli[i] = -1;
-
     while(1){
         memset(cli_buff, 0, sizeof(cli_buff));
         memset(srv_buff, 0, sizeof(srv_buff));
@@ -98,8 +150,7 @@ int main(int argc, char** argv){
             if (cli[i] < 0) continue;
             int sockfd = cli[i];
             if(FD_ISSET(sockfd, &rset)){
-                Read(sockfd, srv_buff, sizeof(srv_buff));
-                printf("%s\n", srv_buff);
+                bbs_main(sockfd);
             }
         }
     }
