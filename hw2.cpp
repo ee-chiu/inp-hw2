242f8557b3feb4ea447b1fad7d8fa93a7bb47bd5
ee-chiu <80535163+ee-chiu@users.noreply.github.com>
2021-11-14 21:54:05 +0800

    register, login, logout, exit (hw1 commands) implemented

--- hw2.cpp
+++ hw2.cpp
@@ -5,6 +5,8 @@
 #include <sys/select.h>
 #include <vector>
 #include <string>
+#include <map>
+#include <queue>
 
 using namespace std;
 
@@ -13,6 +15,9 @@ char srv_buff[10000];
 const int max_size = 20;
 vector<bool> isLogin(max_size, false);
 vector<string> user(max_size, "");
+map<string, string> user2password;
+map< string, map< string, queue<string> > > user2other_user_message;
+
 fd_set all_set;
 
 int char2int(char* c){
@@ -58,14 +63,90 @@ vector<string> split(string command){
     return para;
 }
 
-void write2cli(int connfd,const char * message){
+void write2cli(int sockfd,const char * message){
     snprintf(cli_buff, sizeof(cli_buff), "%s", message);
-    Write(connfd, cli_buff, strlen(cli_buff));
+    Write(sockfd, cli_buff, strlen(cli_buff));
     return;
 }
 
-void welcome(int connectfd){
-    write2cli(connectfd, "********************************\n** Welcome to the BBS server. **\n********************************\n");
+void write2cli2(int sockfd, const char * text, const char * user){
+    snprintf(cli_buff, sizeof(cli_buff), "%s, %s.\n", text, user);
+    Write(sockfd, cli_buff, strlen(cli_buff));
+    return;
+}
+
+void welcome(int sockfd){
+    write2cli(sockfd, "********************************\n** Welcome to the BBS server. **\n********************************\n");
+    return;
+}
+
+void Exit(int sockfd){
+    if(isLogin[sockfd]) {
+        snprintf(cli_buff, sizeof(cli_buff), "Bye, %s.", user[sockfd].c_str());
+        Write(sockfd, cli_buff, strlen(cli_buff));
+    }
+    isLogin[sockfd] = false;
+    user[sockfd].clear();                
+
+    Close(sockfd);
+
+    FD_CLR(sockfd, &all_set);
+
+    return;
+}
+
+void reg(int sockfd, const vector<string> &para){
+    if(para.size() != 3){
+        write2cli(sockfd, "Usage: register <username> <password>\n");
+        return;
+    }
+
+    auto itr = user2password.find(para[1]);
+    if(itr != user2password.end()){
+        write2cli(sockfd, "Username is already used.\n");
+        return;
+    }
+
+    write2cli(sockfd, "Register successfully.\n");
+    user2password[para[1]] = para[2];
+    return;
+}
+
+void login(int sockfd,const vector<string> &para){
+    if(para.size() != 3){
+        write2cli(sockfd, "Usage: login <username> <password>\n");
+        return;
+    }
+
+    if(isLogin[sockfd]){
+        write2cli(sockfd, "Please logout first.\n");
+        return;
+    }
+
+    auto itr = user2password.find(para[1]);
+    if(itr == user2password.end()){
+        write2cli(sockfd, "Login failed.\n");
+        return;
+    }
+
+    if(user2password[para[1]] != para[2]){
+        write2cli(sockfd, "Login failed.\n");
+        return;
+    }
+
+    write2cli2(sockfd, "Welcome", para[1].c_str());
+    isLogin[sockfd] = true;
+    user[sockfd] = para[1];
+}
+
+void logout(int sockfd){
+    if(!isLogin[sockfd]){
+        write2cli(sockfd, "Please login first.\n");
+        return;
+    }
+    isLogin[sockfd] = false;
+    write2cli2(sockfd, "Bye", user[sockfd].c_str());
+    user[sockfd].clear();
     return;
 }
 
@@ -74,19 +155,15 @@ void bbs_main(int sockfd){
     if(srv_buff[0] != 0){
         string command(srv_buff);
         command.pop_back();
-        if(command == "exit"){
-            if(isLogin[sockfd]) {
-                snprintf(cli_buff, sizeof(cli_buff), "Bye, %s.", user[sockfd].c_str());
-                Write(sockfd, cli_buff, strlen(cli_buff));
-            }
-            isLogin[sockfd] = false;
-            user[sockfd].clear();                
-
-            Close(sockfd);
-
-            FD_CLR(sockfd, &all_set);
+        vector<string> para = split(command);
+        if(para[0] == "exit"){
+            Exit(sockfd);
             return;
         }
+
+        if(para[0] == "register") reg(sockfd, para);
+        else if(para[0] == "login") login(sockfd, para);
+        else if(para[0] == "logout") logout(sockfd);
     }
 }
 
